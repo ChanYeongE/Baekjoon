620fd8785bcd4e0ac32477a91aa1670cc95cff43
ChanYeongE <chanyeong.dev@gmail.com>
2022-01-25 14:18:07 +0900

    팩토리얼

---
+++ 10872.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+
+function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    }
+    
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(input));

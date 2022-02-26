67b9cd6cd8e869e7fee37651bae64729b9c808e6
ChanYeongE <chanyeong.dev@gmail.com>
2022-02-26 16:50:41 +0900

    약수

---
+++ 1037.js
@@ -0,0 +1,20 @@
+function sol(input) {
+    const divisors = input[1].split(" ");
+    return Math.min(...divisors) * Math.max(...divisors);
+  }
+  
+  function sol(input) {
+    const divisors = input[1].split(" ").sort((a, b) => a - b);
+    return divisors[0] * divisors[divisors.length - 1];
+  }
+  
+  const input = [];
+  require("readline")
+    .createInterface(process.stdin, process.stdout)
+    .on("line", (line) => {
+      input.push(line);
+    })
+    .on("close", () => {
+      console.log(sol(input));
+      process.exit();
+    });

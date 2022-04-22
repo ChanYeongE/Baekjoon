11f8072ef23ed5109f2ff62e0ac8604dc749cca8
ChanYeongE <chanyeong.dev@gmail.com>
2022-04-22 16:25:29 +0900

    심부름 가는 길

---
+++ 5554.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); 
+
+let answer = 0; 
+
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(i => answer += Number(i)); 
+
+let minute = parseInt(answer/60) 
+let second = parseInt(answer%60) 
+
+let ans = `${minute}\n${second}`; 
+
+console.log(ans)
+
